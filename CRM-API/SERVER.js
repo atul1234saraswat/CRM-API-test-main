59d33b850f28eee8fa1901f8db3597a59b1f8fe8
Atul Saraswat <atul919351@gmail.com>
2025-06-05 15:38:36 +0530

    Update SERVER.js

--- CRM-API/SERVER.js
+++ CRM-API/SERVER.js
@@ -3,12 +3,8 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const userRoutes = require('./routes/userRoutes');
-const supplierRoutes = require('./routes/supplierRoutes');
-
 app.use(express.json());
 app.use('/api/users', userRoutes);
-app.use('/api/suppliers', supplierRoutes);
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
